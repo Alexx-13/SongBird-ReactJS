7ebaf223d97b7c669359307b8eb8510788516eae
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-03-02 15:24:14 +0300

    Updates

---
+++ precache-manifest.9fb213c780e80a4b7a855447d2763a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8abe7f395902356bb951d0ef14691d",
+    "url": "/Alexx-13.github.io/SongBird-ReactJS/index.html"
+  },
+  {
+    "revision": "ce7d5180a3372eadbb5c",
+    "url": "/Alexx-13.github.io/SongBird-ReactJS/static/css/main.3b1c0270.chunk.css"
+  },
+  {
+    "revision": "db924305e2a3c81d348d",
+    "url": "/Alexx-13.github.io/SongBird-ReactJS/static/js/2.744ba625.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Alexx-13.github.io/SongBird-ReactJS/static/js/2.744ba625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7d5180a3372eadbb5c",
+    "url": "/Alexx-13.github.io/SongBird-ReactJS/static/js/main.c670ab15.chunk.js"
+  },
+  {
+    "revision": "cbbbc0dd6f40f7e60221",
+    "url": "/Alexx-13.github.io/SongBird-ReactJS/static/js/runtime-main.848de9e3.js"
+  }
+]);
